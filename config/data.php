615b968b46bad6d49b47fa957a8e1d3808b30b4c
Bob <roberto.lombardi@b-rent.it>
2023-03-03 11:53:05 +0100

    test

--- config/data.php
+++ config/data.php
@@ -1,14 +1,14 @@
 <?php
 
 return array(
-    'host' => 'localhost',
-    'username' => 'flotta_brasiello',
-    'password' => '%Kp3l8%%Kp3l8s13p3l8s13',
-    'database' => 'brasiello_fleet',
     // 'host' => 'localhost',
-    // 'username' => 'brasiello',
-    // 'password' => 'Bras7!ll0',
+    // 'username' => 'flotta_brasiello',
+    // 'password' => '%Kp3l8%%Kp3l8s13p3l8s13',
     // 'database' => 'brasiello_fleet',
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'root',
+    'database' => 'brasiello_fleet',
     // 'hostmailer' => 'ns382386.ip-46-105-99.eu',
     // 'usernamemailer' => 'mailer_script',
     // 'passwordmailer' => '3Rm4js1_3Rm4js1_',
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once ('vendor/autoload.php');
+include 'parts/pages/fun.php';
+
+print_r(tellMeVettore(79088));
