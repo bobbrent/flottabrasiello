2ca91aff4f0ca01973574553d9d8caf1bebe09f4
Bob <58815374+bobbrent@users.noreply.github.com>
2023-09-20 18:18:09 +0200

    Update download.php
    refix

--- uploadbolle/download.php
+++ uploadbolle/download.php
@@ -2,19 +2,19 @@
 
 if (isset($_GET['id'])) 
 {
-  include($_SERVER['DOCUMENT_ROOT'].'/functions/db.php');
+  // include($_SERVER['DOCUMENT_ROOT'].'/functions/db.php');
   $id = $_GET['id'];
-  // $connection = mysqli_connect("localhost", "root", "", "brasiello_fleet");
-  $db = new dbObj();
-  $connString =  $db->getConnstring();
+  $connection = mysqli_connect('localhost', 'flotta_brasiello', '%Kp3l8%%Kp3l8s13p3l8s13', 'brasiello_fleet');
+  // $db = new dbObj();
+  // $connString =  $db->getConnstring();
   $query = "SELECT * " .
           "FROM file_bolle WHERE id = '$id'";
-  $result = mysqli_query($connString,$query) or die('Error, query failed');
+  $result = mysqli_query($connection,$query) or die('Error, query failed');
   list($id, $id_bolla, $file, $content, $size,$type) = mysqli_fetch_array($result);
   header("Content-type: $type");
   header("Content-Disposition: attachment; filename=$file");
 
   echo $content;
-  mysqli_close($connString);
+  mysqli_close($connection);
   exit;
 }
